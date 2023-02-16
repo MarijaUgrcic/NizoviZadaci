e8ce8f1fecb8a879e6985387373dfff6f7fba388
MarijaUgrcic <marija.ugrcic@yahoo.com>
2023-02-16 14:09:50 +0100

    zavresen random zadatak

--- src/RandomPoker.java
+++ src/RandomPoker.java
@@ -7,15 +7,27 @@ public class RandomPoker {
         String[] niz = {"Heart", "Cherry", "Coin", "Melon", "Jocker"}; //pocetni niz
 
         String[] niz2 = {"Jocker", "Melon", "Melon", "Heart", "Coin"}; // ocekivani niz na izlazu
-        String[] niz3 = new String[5]; //niz koji stampamo a koji treba da bude isti kao niz2
+        String[] niz3 = {"Heart", "Cherry", "Coin", "Melon", "Jocker"}; //niz koji stampamo a koji treba da bude isti kao niz2
 
 
-        while (!niz3[0].equals("Jocker") && !niz3[1].equals("Melon") && !niz3[2].equals("Melon")
-                && !niz3[3].equals("Heart") && !niz3[4].equals("Coin")) {
-            for (int i = 0; i < niz3.length; i++) {
-                int index = random.nextInt(niz.length);
-                System.out.println(niz3[index]);
-            }
+        while (!(niz3[0].equals("Jocker") && niz3[1].equals("Melon") && niz3[2].equals("Melon")
+                && niz3[3].equals("Heart") && niz3[4].equals("Coin"))) {
+
+            int index = random.nextInt(niz.length);
+            niz3[0] = niz[index];
+            int index1 = random.nextInt(niz.length);
+            niz3[1] = niz[index1];
+            int index2 = random.nextInt(niz.length);
+            niz3[2] = niz[index2];
+            int index3 = random.nextInt(niz.length);
+            niz3[3] = niz[index3];
+            int index4 = random.nextInt(niz.length);
+            niz3[4] = niz[index4];
+
+        }
+
+        for (int i = 0; i < niz3.length; i++) {
+            System.out.println(niz3[i]);
         }
     }
 }
